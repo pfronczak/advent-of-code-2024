bad4e92e814cc0c3d63d081b353e209711d6aad8
Paweł Fronczak <pfronczak@gmail.com>
2024-12-03 20:22:23 +0100

    changes to file reading

--- day03/day3.test.ts
+++ day03/day3.test.ts
@@ -1,32 +1,25 @@
 import { readFileSync } from 'fs';
 import { day3_1, day3_2 } from './day3';
 
-const sample = readFileSync('day03/sample.txt', 'utf8')
-    .split('\n')
-    .map((line) => line.trim());
-
-const sample2 = readFileSync('day03/sample2.txt', 'utf8')
-    .split('\n')
-    .map((line) => line.trim());
-
-const input = readFileSync('day03/input.txt', 'utf8')
-    .split('\n')
-    .map((line) => line.trim());
+const inputFile = (fileName: string) =>
+    readFileSync(`day03/${fileName}`, 'utf8')
+        .split('\n')
+        .map((line) => line.trim());
 
 describe('Day 3: Mull It Over', () => {
     test('Part 1, example 1', () => {
-        expect(day3_1(sample)).toBe(161);
+        expect(day3_1(inputFile('sample.txt'))).toBe(161);
     });
 
     test('Part 1, puzzle input', () => {
-        expect(day3_1(input)).toBe(185797128);
+        expect(day3_1(inputFile('input.txt'))).toBe(185797128);
     });
 
     test('Part 2, example 1', () => {
-        expect(day3_2(sample2)).toBe(48);
+        expect(day3_2(inputFile('sample2.txt'))).toBe(48);
     });
 
     test('Part 2, puzzle input', () => {
-        expect(day3_2(input)).toBe(89798695);
+        expect(day3_2(inputFile('input.txt'))).toBe(89798695);
     });
 });
